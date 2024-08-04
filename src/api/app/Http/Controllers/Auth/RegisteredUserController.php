95f230b544b5f5b569a6b8109a7fa975748a3c02
Elfry <elfry3@gmail.com>
2024-08-04 20:14:02 +0700

    Implement registration

--- src/api/app/Http/Controllers/Auth/RegisteredUserController.php
+++ src/api/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,8 +34,9 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        // Auth::login($user);
 
-        return response()->noContent();
+        // return response()->noContent();
+        return response($user);
     }
 }
--- src/app/src/pages/RegisterPage.jsx
+++ src/app/src/pages/RegisterPage.jsx
@@ -1,12 +1,37 @@
 import { ChevronLeft } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
 import { PersonAdd } from 'react-bootstrap-icons';
+import { useRef } from 'react';
+import axiosInstance from '../utils/axios-instance.js';
 import Button from 'react-bootstrap/Button';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
-import { Link } from 'react-router-dom';
 import Stack from 'react-bootstrap/Stack';
 
 export default function RegisterPage () {
+
+  const nameRef = useRef();
+  const usernameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const user = {
+      name: nameRef.current.value,
+      username: usernameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value,
+    };
+
+    axiosInstance.post('/register', user)
+      .then(({data}) => {
+        console.log(data);
+      });
+  }
 
   return <>
     <Link to="/">
@@ -14,30 +39,57 @@ export default function RegisterPage () {
         <ChevronLeft /><span className="ms-2">Back</span>
       </Button>
     </Link>
-    <FloatingLabel label="Username" className="mt-3">
-      <Form.Control type="text" placeholder="Username" autoFocus/>
-    </FloatingLabel>
-    <FloatingLabel label="Email address" className="mt-3">
-      <Form.Control
-        type="email"
-        placeholder="name@example.com"
-        aria-describedby="emailHelpBlock"
-      />
-      <Form.Text id="emailHelpBlock">
-        Verification links will be sent to this address
-      </Form.Text>
-    </FloatingLabel>
-    <FloatingLabel label="Password" className="mt-3">
-      <Form.Control type="password" placeholder="Password" />
-    </FloatingLabel>
-    <FloatingLabel label="Confirm password" className="mt-3">
-      <Form.Control type="password" placeholder="Password" />
-    </FloatingLabel>
-    <Stack className="mt-3" direction="horizontal">
-      <Button className="ms-auto" variant="primary">
-        <PersonAdd /><span className="ms-2">Register</span>
-      </Button>
-    </Stack>
-
+    <Form>
+      <FloatingLabel label="Name" className="mt-3">
+        <Form.Control
+          type="text"
+          placeholder="Full name"
+          ref={nameRef}
+          autoFocus
+        />
+      </FloatingLabel>
+      <FloatingLabel label="Username" className="mt-3">
+        <Form.Control
+          type="text"
+          placeholder="Username"
+          ref={usernameRef}
+        />
+      </FloatingLabel>
+      <FloatingLabel label="Email address" className="mt-3">
+        <Form.Control
+          type="email"
+          placeholder="name@example.com"
+          aria-describedby="emailHelpBlock"
+          ref={emailRef}
+        />
+        <Form.Text id="emailHelpBlock">
+          Verification links will be sent to this address
+        </Form.Text>
+      </FloatingLabel>
+      <FloatingLabel label="Password" className="mt-3">
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          ref={passwordRef}
+        />
+      </FloatingLabel>
+      <FloatingLabel label="Confirm password" className="mt-3">
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          ref={passwordConfirmationRef}
+        />
+      </FloatingLabel>
+      <Stack className="mt-3" direction="horizontal">
+        <Button
+          type="submit"
+          className="ms-auto"
+          variant="primary"
+          onClick={handleSubmit}
+        >
+          <PersonAdd /><span className="ms-2">Register</span>
+        </Button>
+      </Stack>
+    </Form>
   </>
 }
